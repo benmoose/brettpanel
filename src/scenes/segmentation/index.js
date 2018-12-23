6c548d3ebf8b2e8f0133b0c484d20ae5c53ae4b3
Ben Hadfield <ben.hadfield@citymapper.com>
2018-12-23 22:53:45 +0000

    fix typo

--- src/scenes/segmentation/index.js
+++ src/scenes/segmentation/index.js
@@ -97,7 +97,7 @@ export default class Segmentation extends React.Component {
               <div style={{display: "flex", alignItems: "baseline"}}>
                 <Label>Access Key</Label>
                 <small style={{marginLeft: "5px", opacity: 0.65}}>
-                  This is destroyed then you close or refresh the page, it's only used to authenticate with Mixpanel
+                  This is lost when you close or refresh the page, it's only used to authenticate with Mixpanel
                 </small>
               </div>
               <InputGroup
